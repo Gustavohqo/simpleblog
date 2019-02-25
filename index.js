d2c1ce8f139375085ed208e0c5fb9381aceefad5
Gustavo Oliveira <gustavo.hqo@gmail.com>
2019-02-25 13:35:09 -0300

    adicionado evento de mudança de conteúdo no sample do markdown

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+function bindLocalVariable(data, callback) {
+
+    return callback ? callback : () => {
+        console.log(data);
+    };
+}
+
 ((showdown) => {
     const document = window.document;
+
+    
 
     var bodyNode = document.getElementsByClassName('sample')[0];
     var showdown = new showdown.Converter(),
@@ -9,6 +18,14 @@
 ## Title 2
 ### Title 3`;
     
+    window['changeTextValue'] = () => {
+            console.log(text);
+            text = document.getElementsByTagName('textarea')[0].value;
+
+            htmlObject.innerHTML =  showdown.makeHtml(text);    
+            bodyNode.appendChild(htmlObject);
+        };
+
     var htmlObject = document.createElement('div');
     htmlObject.innerHTML =  showdown.makeHtml(text);    
     bodyNode.appendChild(htmlObject);
